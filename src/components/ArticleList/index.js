fc35b6fc47eae791b6a56cfcce7591fe7cd94936
Vladimir <vladimir.ivinski89@gmail.com>
2021-05-04 12:50:14 +0300

    remove AppList add style

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Article from './Article';
+import Article from '../Article';
+import './style.css';
 
 export default function ArticleList({ articles }) {
   const articleElements = articles.map((article) => (
-    <li key={article.id}>
+    <li key={article.id} className='article-list__li'>
       <Article article={article} />
     </li>
   ));
